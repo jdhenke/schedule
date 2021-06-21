fea991fcd35de4537a1f7104ebd963585c490799
Joe Henke <joed.henke@gmail.com>
2021-06-21 14:56:16 -0400

    Switch to golp

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module schedule
 
 go 1.16
 
-require gonum.org/v1/gonum v0.9.2
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/draffensperger/golp v0.0.0-20191223134725-6feefe64f766
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -2,67 +2,124 @@ package main
 
 import (
 	"fmt"
-	"gonum.org/v1/gonum/mat"
-	"gonum.org/v1/gonum/optimize/convex/lp"
+	"github.com/draffensperger/golp"
 	"log"
 )
 
 const (
-	numDays = 7
+	numDays = 30
 )
+
+type Constraints struct {
+	StretchLimit int
+	NumShifts    float64
+	IsAvailable  func(day int) bool
+}
 
 var (
 	// (person i) is available on these given shifts
-	availabilityByPerson = map[int][]int{
-		0: {0, 1, 2, 3},
-		1: {4, 5, 6},
-		2: {0, 2, 4, 6},
+	constraintsByPerson = map[int]Constraints{
+		0: {
+			StretchLimit: 3,
+			NumShifts:    7,
+			IsAvailable: func(day int) bool {
+				return day < 14
+			},
+		},
+		1: {
+			StretchLimit: 4,
+			NumShifts:    8,
+			IsAvailable: func(day int) bool {
+				return day > 7 && day < 21
+			},
+		},
+		2: {
+			StretchLimit: 5,
+			NumShifts:    7,
+			IsAvailable: func(day int) bool {
+				return day > 14
+			},
+		},
+		3: {
+			StretchLimit: 2,
+			NumShifts:    8,
+			IsAvailable: func(day int) bool {
+				return true
+			},
+		},
 	}
-	numPeople = len(availabilityByPerson)
+	numPeople = len(constraintsByPerson)
 	numVars   = numPeople * numDays
 )
 
 func main() {
-	// just want *A* solution for now
-	c := make([]float64, numVars)
-
-	// will assemble constraints here
-	var A []float64
-	var b []float64
+	lp := golp.NewLP(0, numVars)
 
 	// ensure no one is scheduled when they are not available
-	for person, availability := range availabilityByPerson {
-		// row of A ensuring they are not working when they are NOT available
+	for person, constraints := range constraintsByPerson {
+		// ensure they don't have stretches that are too long
+		{
+			for start := 0; start+constraints.StretchLimit < numDays; start++ {
+				row := make([]float64, numVars)
+				for x := 0; x < constraints.StretchLimit; x++ {
+					row[getIndex(person, start+x)] = 1
+				}
+				if err := lp.AddConstraint(row, golp.LE, float64(constraints.StretchLimit)-1); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+
+		// ensure they have the right number of shifts
+		{
+			row := make([]float64, numVars)
+			for day := 0; day < numDays; day++ {
+				row[getIndex(person, day)] = 1
+			}
+			if err := lp.AddConstraint(row, golp.EQ, constraints.NumShifts); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		// ensure they are only scheduled when available
+		{
+			row := make([]float64, numVars)
+			for day := 0; day < numDays; day++ {
+				val := float64(1)
+				if constraints.IsAvailable(day) {
+					val = 0
+				}
+				row[getIndex(person, day)] = val
+			}
+			if err := lp.AddConstraint(row, golp.EQ, 0); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	// ensure one person is scheduled at all times
+	for day := 0; day < numDays; day++ {
 		row := make([]float64, numVars)
+		for person := 0; person < numPeople; person++ {
+			row[getIndex(person, day)] = 1
+		}
+		if err := lp.AddConstraint(row, golp.EQ, 1); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// force integers
+	for person := 0; person < numPeople; person++ {
 		for day := 0; day < numDays; day++ {
-			row[getIndex(person, day)] = 1
+			lp.SetInt(getIndex(person, day), true)
 		}
-		for _, day := range availability {
-			row[getIndex(person, day)] = 0
-		}
-		A = append(A, row...)
-		b = append(b, 0)
 	}
 
-	// ensure someone is scheduled at all times
-	for day := 0; day < numDays; day++ {
-		row := make([]float64, numVars)
-		for person := 0; person < numPeople; person++ {
-			row[getIndex(person, day)] = 1
-		}
-		A = append(A, row...)
-		b = append(b, 1)
-	}
-
-	// solve and print solution
-	matA := mat.NewDense(len(A)/numVars, numVars, A)
-	_, x, err := lp.Simplex(c, matA, b, 0, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	lp.SetObjFn(make([]float64, numVars))
+	fmt.Println(lp.Solve())
 	for day := 0; day < numDays; day++ {
 		for person := 0; person < numPeople; person++ {
-			if x[getIndex(person, day)] == 1 {
+			if lp.Variables()[getIndex(person, day)] > 0 {
 				fmt.Printf("Day %d --> Person %d\n", day, person)
 			}
 		}
